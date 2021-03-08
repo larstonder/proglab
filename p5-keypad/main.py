77ee55662ae0d84b10baf2b53fcd36a12b4bc0b4
haved <krogstie.havard@gmail.com>
2021-03-08 15:19:02 +0100

    legge til keypad

--- p5-keypad/main.py
+++ p5-keypad/main.py
@@ -1,4 +1,24 @@
+#!/usr/bin/env python3
+
 '''Main keypad module'''
 
-class Main:
-    '''Main class'''
+from time import sleep
+from keypad import Keypad
+from terminal import disable_echo
+
+def main():
+    '''main function for keypad program'''
+
+    # Turn off repeating input
+    disable_echo()
+    keypad = Keypad()
+
+    while True:
+        press = keypad.poll()
+        if press is not None:
+            print(press)
+        sleep(0.016)
+
+
+if __name__ == "__main__":
+    main()
