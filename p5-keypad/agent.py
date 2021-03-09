958aaf87c6fcf253e94c654d59e4aba048407187
larstonder <lars.tonder@outlook.com>
2021-03-09 11:58:00 +0100

    fix: changed word to code

--- p5-keypad/agent.py
+++ p5-keypad/agent.py
@@ -11,12 +11,6 @@ class Agent:
         self.override_signal = None
         self.passcode_buffer = ''
     
-    def reset_passcode_entry(self):
-        '''Clear the passcode-buffer and initiate a “power up” lighting
-        sequence on the LED Board.'''
-        self.reset_password_accumulator()
-        self.ledboard.power_on_animation()
-    
     def get_next_signal(self):
         '''Returns the override signal, if it is non-blank; otherwise query the
         keypad for the next pressed key'''
@@ -24,26 +18,28 @@ class Agent:
         self.override_signal = None
         return self.keypad.get_next_signal() if temp is None else temp
     
-    def reset_password_accumulator(self):
-        """Resets password accumulator"""
+    def prepare_passcode_entry(self):
+        '''Clear the passcode-buffer and initiate a “power up” lighting
+        sequence on the LED Board.'''
         self.passcode_buffer = ''
         self.override_signal = None
+        self.ledboard.power_on_animation()
     
-    def append_next_password_digit(self, digit):
-        """Appends next password digit"""
+    def append_next_passcode_digit(self, digit):
+        """Appends next passcode digit"""
         assert signal_is_digit(digit)
         self.passcode_buffer += digit
     
     def verify_login(self):
-        '''Checks that the password just entered via the keypad matches that in the
-        password file. Stores the result (Y or N) in the override signal. Calls the
+        '''Checks that the passcode just entered via the keypad matches that in the
+        passcode file. Stores the result (Y or N) in the override signal. Calls the
         LED Board to initiate the appropriate lighting pattern for login success or failure'''
         # self.passcode_buffer
     
-    def validate_passcode_change(self, new_password):
-        '''Checks that the new password is legal. Writes the new
-        password in the password file. Uses the LED
-        Board to signal success or failure in changing the password'''
+    def validate_passcode_change(self, new_passcode):
+        '''Checks that the new passcode is legal. Writes the new
+        passcode in the passcode file. Uses the LED
+        Board to signal success or failure in changing the passcode'''
     
     def light_one_led(self, led):
         '''Using values stored in the Lid and Ldur slots, calls the LED Board and
@@ -58,6 +54,6 @@ class Agent:
         '''Call the LED Board and request the twinkling of all LEDs.'''
         self.ledboard.twinkle_all_leds()
     
-    def exit_action(self):
+    def reset_keypad(self):
         '''Call the LED Board to initiate the “power down” lighting sequence.'''
         self.ledboard.power_off_animation()
