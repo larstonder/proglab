c8ebb775958275034d77917682ca328625347984
larstonder <lars.tonder@outlook.com>
2021-04-06 12:30:40 +0200

    fix of isomap algorithm

--- p6-visualization/isomap_v2.py
+++ p6-visualization/isomap_v2.py
@@ -1,12 +1,15 @@
 import numpy as np
-import scipy.linalg as sp
+# import scipy.sparse.linalg as sp
+# import scipy.linalg as sp
 import sklearn.utils.graph_shortest_path as sg
 from dimred import DimRed
 from kNN import k_nearest
+from scipy.linalg import eigh
+from scipy.sparse.linalg import eigs
 
 class IsoMap2(DimRed):
 
-    k = 25
+    k = 15
     m = 2
 
     def get_description(self):
@@ -16,35 +19,32 @@ class IsoMap2(DimRed):
 
     def reduce_dimensions(self, dataset):
         """Reduces the dimensions of the dataset"""
-        numpoints = len(dataset)
-        geodesics = self.__compute_geodesics(dataset, self.k)
-        result = self.__MDS(geodesics, numpoints)
-        # result = self.__MDS(geodesics, numpoints)
+        geodesics = self.__compute_geodesics(dataset)
+        result = self.__MDS(geodesics)
         return(result)
 
-    def __compute_geodesics(self, dataset, k):
+    def __compute_geodesics(self, dataset):
         """Takes high-dimensional data and a user specified parameter k as input, and returns a distance
         matrix D, where D_ij is the shortest-path distance between x_i and x_j along the manifold"""
-        distance_matrix = k_nearest(dataset, k)
+        distance_matrix = k_nearest(dataset, self.k)
         return sg.graph_shortest_path(distance_matrix)
     
-    def __MDS(self, dataset, n):
+    def __MDS(self, dataset):
         """Applies classical multidimensional scaling over a geodesic distance matrix, and returns
         the coordinates of the low-dimensional mapped points y_1 ... y_n"""
+        numpoints = np.size(dataset, 0)
         squared_proximity_matrix = np.square(dataset)
-        centered = self.__double_centering(squared_proximity_matrix, n)
-        eigenvalues, eigenvectors = sp.eigh(centered, eigvals=(n-self.m, n-1))
+        double_centered = self.__double_centering(squared_proximity_matrix, numpoints)
+        # eigenvalues, eigenvectors = eigh(double_centered, eigvals=(numpoints-self.m, numpoints-1))
+        eigenvalues, eigenvectors = eigs(double_centered, k=self.m)
         l = np.diag(eigenvalues)
-        y = np.dot(eigenvectors, l**(1/2))
-        return y
+        y = eigenvectors @ np.sqrt(l)
+        return np.real(y)
     
     def __double_centering(self, squared, n):
         """Performs double centering using centering matrix j, where n is the number of datapoints"""
         identity_matrix = np.identity(n)
-        one_vector = np.ones((1, n))
-        ones = one_vector.dot(one_vector.T)
-        # j = identity_matrix - (1/n) * ones
-        j = np.subtract(identity_matrix, np.multiply(1/n, ones))
-        # b = -(1/2) * j * squared
-        b = np.dot(np.multiply(-(1/2),j), squared)
+        ones = np.ones(squared.shape)
+        j = identity_matrix - (1/n) * ones
+        b = -(1/2) * j @ squared @ j
         return b
