0f5c67afd4ecf2e0f6b1087d74b83d826691036e
Magnus Lykke Dahlø <magnusld@stud.ntnu.no>
2021-04-06 13:42:40 +0200

    Legge til resten av funksjonalitet, tror alt fungerer som det skal

--- p6-visualization/pca.py
+++ p6-visualization/pca.py
@@ -8,6 +8,7 @@ class PCA:
     """Implementation of the Principal Component Analysis"""
     dataset = None
     length_of_data = None
+    d = 2
 
     def __init__(self, dataset):
         self.dataset = dataset
@@ -16,13 +17,26 @@ class PCA:
     def fit(self):
         """Method for fiting a PCA model for the dataset"""
         centered_data = self.center_data()
+        D = len(centered_data[0])
         covarance_matrix_sigma = np.cov(centered_data, self.dataset)
         transformation_matrix = np.array()
-        #TODO Finne ut hva D og d er, filtrere å legge til de riktige eigenvektorene i transformasjons matrisen.
-        if D - 1 > d:
+        #Tror D er dimensjonen på covariance matrisen, og d er ønsket dimensjon
+        if D - 1 > self.d:
             eigvals, eigvecs = sp.sparse.linalg.eigs(covarance_matrix_sigma)
-        elif D - 1 == d:
+        elif D - 1 == self.d:
             eigvals, eigvecs = np.linalg.eigh(covarance_matrix_sigma)
+        eignvals_with_eigenvecs = []
+        for i in range(len(eigvals)):
+            eignvals_with_eigenvecs.append((eigvals[i], eigvecs[i]))
+        for i in range(self.d):
+            largest_eigenval = 0
+            index_of_largest_eigenval = 0
+            for j in range(len(eignvals_with_eigenvecs)):
+                if eignvals_with_eigenvecs[j][0] > largest_eigenval:
+                    largest_eigenval = eignvals_with_eigenvecs[j][0]
+                    index_of_largest_eigenval = j
+            np.append(transformation_matrix, eignvals_with_eigenvecs[index_of_largest_eigenval][1])
+            eignvals_with_eigenvecs.pop(index_of_largest_eigenval)
 
         return transformation_matrix, centered_data
 
@@ -36,7 +50,7 @@ class PCA:
     def center_data(self):
         """Method for centering the data"""
         my = self.dataset.sum / self.length_of_data
-        centered_data = np.array()
+        centered_data = np.array() 
         for datapoint in self.dataset:
             centered_point = np.subtract(datapoint, my)
             np.append(centered_data, centered_point, axis=0)
