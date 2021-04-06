831f922c6ce587a3a0333a7371c3a0c39fc1302f
Magnus Lykke Dahlø <magnusld@stud.ntnu.no>
2021-04-07 00:06:19 +0200

    Rette opp i noen lint erroris i tsne.py

--- p6-visualization/tsne.py
+++ p6-visualization/tsne.py
@@ -1,13 +1,14 @@
 
 """Implementation of t-SNE"""
 
-import numpy as np
 from time import time
 from datetime import datetime
 from sys import stdout
+import numpy as np
 
 from dimred import DimRed
 from kNN import pairwise_squared_dist, symmetric_binary_k_nearest
+
 
 class TSNE(DimRed):
     """t-SNE implementation class"""
@@ -34,8 +35,8 @@ class TSNE(DimRed):
         print("Picking random starting values for Y")
         np.random.seed(self.seed)
         Y = np.random.normal(loc=0, scale=10**-4, size=(n, 2))
-        g = np.full((n,2), 1)
-        delta = np.full((n,2), 0)
+        g = np.full((n, 2), 1)
+        delta = np.full((n, 2), 0)
 
         start = time()
         print("Starting iteration at:", datetime.now().strftime("%H:%M:%S"))
@@ -45,17 +46,17 @@ class TSNE(DimRed):
 
             # y_j is a NxN matrix with element i,j = y_j (aka a point)
             # This means the row is "ignored" (all rows are identical)
-            y_j = np.array([Y,]*n)
+            y_j = np.array([Y, ] * n)
 
             # y_i is independent on j, so all columns are equal
             y_i = np.swapaxes(y_j, 0, 1)
 
             p_mul = self.p_multiplier(iteration)
 
-            p_q_diff = p_mul*p/p_sum - q/q_sum
-            p_q_diff_times_q = p_q_diff * q #(2000,2000)
-            p_q_diff_times_q = p_q_diff_times_q.reshape((n,n,1))
-            y_i_j_diff = y_i - y_j #(2000,2000,2)
+            p_q_diff = p_mul * p / p_sum - q / q_sum
+            p_q_diff_times_q = p_q_diff * q  # (2000,2000)
+            p_q_diff_times_q = p_q_diff_times_q.reshape((n, n, 1))
+            y_i_j_diff = y_i - y_j  # (2000,2000,2)
             product = p_q_diff_times_q * y_i_j_diff
             gradient = 4 * (product).sum(axis=1)
 
@@ -63,21 +64,22 @@ class TSNE(DimRed):
             g_times = g * 0.8
 
             use_plus = np.sign(delta) != np.sign(gradient)
-            all_true = np.full((n,2), True)
+            all_true = np.full((n, 2), True)
             g = np.select([use_plus, all_true], [g_plus, g_times])
             g = np.maximum(g, 0.01)
 
-            delta = self.alpha(iteration)*delta - self.epsilon*g*gradient
+            delta = self.alpha(iteration) * delta - self.epsilon * g * gradient
             Y = Y + delta
 
-            done = iteration+1
+            done = iteration + 1
             if done % 10 == 0:
-                time_spent = time()-start
+                time_spent = time() - start
                 time_per = time_spent / done
-                estimate = (self.max_iterations-done)*time_per
-                print(f"\rProgress: {done}/{self.max_iterations}" +
-                      f"    [{time_spent:.3f}s]  est {estimate:.0f}s left", end="")
+                estimate = (self.max_iterations - done) * time_per
+                print(
+                    f"\rProgress: {done}/{self.max_iterations}" +
+                    f"    [{time_spent:.3f}s]  est {estimate:.0f}s left",
+                    end="")
                 stdout.flush()
-
 
         return Y
